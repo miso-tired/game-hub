0db4650b496534ced41a86f91f459c721892dba8
Anthony <anthonylsa12@gmail.com>
2024-07-31 21:22:28 -0700

    Game cards now display metacritic scores with a color depending on the score.

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,15 @@
+import { Badge } from '@chakra-ui/react'
+
+interface Props {
+    score: number
+}
+
+const CriticScore = ({ score }: Props) => {
+    let color = score > 75 ? 'green' : score > 60 ? 'yellow' : 'orange';
+
+  return (
+    <Badge colorScheme={color} fontSize='medium' paddingX='10px'>{score}</Badge>
+  )
+}
+
+export default CriticScore
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, HStack } from "@chakra-ui/react";
 import { Game } from "../hooks/UseGame";
 import PlatformIcon from "./PlatformIcon";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -12,9 +13,12 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <HStack justifyContent='space-between'>
         <PlatformIcon
           platforms={game.parent_platforms.map((p) => p.platform)}
         />
+        <CriticScore score={game.metacritic}></CriticScore>
+        </HStack>
       </CardBody>
     </Card>
   );
