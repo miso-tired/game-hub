dd11afb03cc61faefe37dd897e9f08c2ed274762
Anthony <anthonylsa12@gmail.com>
2024-08-01 18:01:25 -0700

    Got rid of duplicate styles for the Card components in GameCardSkeleton and GameCard.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,19 +1,35 @@
-import { SimpleGrid, Text } from "@chakra-ui/react"
-import useGame from "../hooks/UseGame"
-import GameCard from "./GameCard"
-
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGame from "../hooks/UseGame";
+import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-     const {games, error} = useGame()
+  const { games, error, loading } = useGame();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
     <>
-    {error && <Text>{error}</Text>}
-    <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} padding='10px' spacing='10px'>
-        {games.map(game => <GameCard key={game.id} game={game} />)}
-    </SimpleGrid>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4, "2xl": 5 }}
+        padding="10px"
+        spacing="10px"
+      >
+        {loading &&
+          skeletons.map((skeletons) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={skeletons} />
+            </GameCardContainer>
+          ))}
+        {games.map((game) => (
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
+        ))}
+      </SimpleGrid>
     </>
-  )
-}
+  );
+};
 
-export default GameGrid
+export default GameGrid;
